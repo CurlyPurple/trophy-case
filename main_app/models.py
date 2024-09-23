1de68aedc5f56ff86fb5975ee44a7f8550dcdef8
AlexG <Alexanderg.savitr@gmail.com>
2024-09-23 15:10:58 -0500

    clean up code

--- main_app/models.py
+++ main_app/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.urls import reverse
 from django.core.validators import MaxValueValidator, MinValueValidator
+from django.contrib.auth.models import User
 
 class Goal(models.Model):
   name = models.CharField(max_length=50)
@@ -11,10 +12,11 @@ class Goal(models.Model):
       MinValueValidator(1)
     ]
   )
+  user = models.ForeignKey(User, on_delete=models.CASCADE)
 
   def __str__(self):
     return self.name
   
   def get_absolute_url(self):
-      return reverse("goal_detail", kwargs={"goal_id": self.id})
+      return reverse("goal-detail", kwargs={"goal_id": self.id})
   
--- main_app/views.py
+++ main_app/views.py
@@ -1,29 +1,54 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from .models import Goal
+from django.contrib.auth.views import LoginView
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
 
-def home(request):
-  return render(request, 'home.html')
+class Home(LoginView):
+  template_name = 'home.html'
 
 def about(request):
   return render(request, 'about.html')
 
+@login_required
 def goal_index(request):
-  goals = Goal.objects.all()
+  goals = Goal.objects.filter(user=request.user)
   return render(request, 'goals/index.html', { 'goals': goals})
 
+@login_required
 def goal_detail(request, goal_id):
   goal = Goal.objects.get(id=goal_id)
   return render(request, 'goals/detail.html', { 'goal': goal })
 
-class GoalCreate(CreateView):
+class GoalCreate(LoginRequiredMixin, CreateView):
   model = Goal
-  fields = '__all__'
+  fields = ['name', 'description', 'determination']
 
-class GoalUpdate(UpdateView):
+  def form_valid(self, form):
+    form.instance.user = self.request.user
+    return super().form_valid(form)
+
+class GoalUpdate(LoginRequiredMixin, UpdateView):
   model = Goal
-  fields = '__all__'
+  fields = ['name', 'description', 'determination']
 
-class GoalDelete(DeleteView):
+class GoalDelete(LoginRequiredMixin, DeleteView):
   model = Goal
   success_url = '/goals/'
+
+def signup(request):
+  error_message = ''
+  if request.method == 'POST':
+    form = UserCreationForm(request.POST)
+    if form.is_valid():
+      user = form.save()
+      login(request, user)
+      return redirect('goal-index')
+    else:
+      error_message = 'Invalid sign up - try again'
+  form = UserCreationForm()
+  context = {'form': form, 'error_message': error_message}
+  return render(request, 'signup.html', context)
